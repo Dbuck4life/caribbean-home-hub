17c4e7e425dc306f1743b82dd3f900d17acab906
Dbuck4life <mrdarrenbuckner@gmail.com>
2025-05-31 11:58:43 -0500

    Update next.config.js to fix build output

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  distDir: '.next',
+  distDir: 'build',
   trailingSlash: true,
   images: {
     unoptimized: true
